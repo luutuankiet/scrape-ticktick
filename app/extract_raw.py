de8204da0534d5020ad22aab295f85bb014bce08
luutuankiet <luutuankiet.ftu2@gmail.com>
2024-02-12 22:35:04 +0700

    working code load func with caching. has a bug on the cache adding duplicate but acceptable amount

--- app/extract_raw.py
+++ app/extract_raw.py
@@ -1,10 +1,14 @@
+from doctest import debug
+from enum import unique
+from unittest.util import strclass
+from pytz import utc
 from ticktick.oauth2 import OAuth2        # OAuth2 Manager
 from ticktick.api import TickTickClient   # Main Interface
 from os import environ
 from dotenv import load_dotenv
 import json
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta,timezone
 import logging
 
 # setup 
@@ -22,52 +26,120 @@ auth_client = OAuth2(client_id=client_id,
 client = TickTickClient(username, password, auth_client)
 
 
-new_tasks=client.state['tasks']
-completed_tasks=[]
-start = datetime(2022, 7, 23)
-file_path = 'raw/all_tasks.json'
 
-def get_completed_tasks(start=start, end=datetime.now(), full_load=True) -> list:
+default_start = datetime(2022, 7, 23,tzinfo=timezone.utc)
+tasks_file_path = 'raw/all_tasks.json'
+lists_file_path = 'raw/all_lists.json'
+folders_file_path = 'raw/all_folders.json'
+date_format = '%Y-%m-%dT%H:%M:%S.%f%z'
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+def deduplicate(source) -> list:
+    """
+    checks each item and remove duplicated
+    """
+    unique_items={}
+    unique_list=[]
+
+    for item in source:
+        item_id=item.get("id")
+
+        if item_id not in unique_items:
+            unique_list.append(item)
+            unique_items[item_id]=True
+    return unique_list
+
+
+def _get_completed_tasks(start=None, end=datetime.now(timezone.utc), full_load=True):
     """_summary_
-    uses tickpy to grab completed tasks from start > end.
+    returns a json string
+    internal func - uses tickpy to grab completed tasks from start > end.
     `start` has a default value 2022/07/23 which is the start of my ticktick interactions
     `end` defaults to the runtime date.
-
-    Args:
-        start (_type_, optional): _description_. Defaults to start.
-        end (_type_, optional): _description_. Defaults to datetime.now().
-        full_load (bool, optional): _description_. Defaults to True.
 
     Returns:
         list: all the completed tasks in the interval.
     """
+    
+    completed_tasks=[] 
     logging.info('start loading tasks')
     if full_load:
-        current_date = start
-    else: 
-        current_date = datetime.now()
-        with open(file_path,'r') as f:
-            completed_tasks=json.load(f)
+        current_date=default_start
+    elif not full_load: 
+        current_date=start
     while current_date <= end:
         tasks=client.task.get_completed(current_date)
         if tasks != []:
             for task in tasks:
                 completed_tasks.append(task)
-            print(f'loaded {len(tasks)} new tasks from {current_date}. next interation...')
+            logging.info(f'loaded {len(tasks)} new tasks from {current_date}. next interation...')
         current_date += timedelta(days=1)
+    # completed_tasks=json.dumps(completed_tasks)
     return completed_tasks
 
+def get_completed_task() -> list:
+    """
+    returns a list the full completed tasks and utilize existing file as cache if available.
+    """
+    try:
+        with open(tasks_file_path,'r') as f:
+            cached=json.load(f)
+            cached_completed=[item for item in cached if 'completedTime' in item]
+            last_cached_date=[item['completedTime'] for item in cached if 'completedTime' in item]
+            last_cached_date=max(last_cached_date)
+            last_cached_date=datetime.strptime(last_cached_date,date_format)
+            # last_cached_date=last_cached_date - timedelta(days=1)
+            full_load=False
+    except FileNotFoundError:
+        logging.info('no cache found. doing full load...')
+        cached_completed=[]
+        last_cached_date=None
+        full_load=True
+    
+    # checks existing and append to cached list 
+    net_new=_get_completed_tasks(start=last_cached_date,full_load=full_load)
+
+    # concatenate final completed list
+    all_completed_tasks=net_new+cached_completed
+    all_completed_tasks=deduplicate(all_completed_tasks)
+    return all_completed_tasks
+    
+
+def get_new_tasks() -> list:
+    new_tasks=client.state['tasks']
+    return new_tasks
+
+def get_all_tasks() -> list:
+    new=get_new_tasks()
+    logging.info(f'new tasks : {len(new)}')
+    completed=get_completed_task()
+    logging.info(f'completed tasks : {len(completed)}')
+    all_tasks=new+completed
+    return all_tasks
 
 def get_lists_and_folders():
-    projects = client.state['projects']
+    lists = client.state['projects']
     folders = client.state['project_folders']
-    return projects, folders
+    return lists, folders
 
-def dump_raw():
-    all_tasks=completed_tasks+new_tasks
-    # TODO : complete the dump interatino 
+def dump_to_file(source:list, target:str):
+    """
+    takes source then dumps to json raw file 
+    """
 
-    with open(file_path,'w') as f:
-        json.dump(all_tasks,f,indent=4)
+    with open(target,'w') as f:
+        json.dump(source,f,indent=4,)
 
+lists,folders = get_lists_and_folders()
+tasks=get_all_tasks()
+
+
+dump_to_file(lists,lists_file_path)
+dump_to_file(folders,folders_file_path)
+dump_to_file(tasks,tasks_file_path)
 
