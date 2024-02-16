10d1b986526e1c3c7ad31270e419509f37834f6d
luutuankiet <luutuankiet.ftu2@gmail.com>
2024-02-17 02:27:15 +0700

    add devcontainer

---
+++ .devcontainer/postAttachCommand.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source .venv/bin/activate
+dagster-webserver -h 0.0.0.0 -p 3000
--- app/ETL/definitions.py
+++ app/ETL/definitions.py
@@ -1,19 +1,23 @@
 import os
-from dagster import Definitions
+from dagster import Definitions, AssetSelection,ScheduleDefinition,define_asset_job,load_assets_from_modules
 from dagster_dbt import DbtCliResource
-from .dbt_assets import ticktick_dbt_assets
+# from .dbt_assets import ticktick_dbt_assets
+
 from .constants import DBT_PROJECT_DIR
-from .EL import get_all_tasks,dump_to_file,get_lists,get_folders,dump_to_motherduck
+# from .EL import get_all_tasks,dump_to_file,get_lists,get_folders,dump_to_motherduck
+from. import EL,dbt_assets
+
+all_assets = load_assets_from_modules([EL,dbt_assets])
+# ETL_job = define_asset_job("ETL_job",selection=AssetSelection.all())
+ETL_job = define_asset_job("ETL_job",selection=all_assets)
+ETL_schedule = ScheduleDefinition(
+    job=ETL_job,
+    cron_schedule="0 * * * *",
+)
 
 defs = Definitions(
-    assets=[
-            get_all_tasks,
-            get_lists,
-            get_folders,
-            dump_to_file,
-            dump_to_motherduck,
-            ticktick_dbt_assets
-            ],
+    assets=all_assets,
+    schedules=[ETL_schedule],
     resources={
         "dbt": DbtCliResource(project_dir=DBT_PROJECT_DIR),
     },
