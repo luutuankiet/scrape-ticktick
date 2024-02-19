6178d87121c3e59bca9cb778eca125d7625b37f8
luutuankiet <luutuankiet.ftu2@gmail.com>
2024-02-19 17:46:25 +0700

    fix weird bug that duplicated the models folder

--- models/marts/core/dim_dates.sql
+++
@@ -1,9 +0,0 @@
-WITH source AS (
-    SELECT *
-    FROM
-        {{ ref('stg_pg__dates') }}
-)
-
-SELECT *
-FROM
-    source
--- models/marts/core/dim_lists.sql
+++
@@ -1,9 +0,0 @@
-WITH stg_lists AS (
-    SELECT *
-    FROM
-        {{ ref ('stg_pg__lists') }}
-)
-
-SELECT *
-FROM
-    stg_lists
