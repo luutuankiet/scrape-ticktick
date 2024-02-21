60259fcd9a83916b37073156db7d8a05b530bc3d
luutuankiet <luutuankiet.ftu2@gmail.com>
2024-02-21 12:38:50 +0700

    job runs every 30m in the specified hours

--- app/ETL/definitions.py
+++ app/ETL/definitions.py
@@ -12,7 +12,7 @@ all_assets = load_assets_from_modules([EL,dbt_assets])
 ETL_job = define_asset_job("ETL_job",selection=all_assets)
 ETL_schedule = ScheduleDefinition(
     job=ETL_job,
-    cron_schedule="0 11,12,14,16,20,22,0 * * *",execution_timezone="Asia/Bangkok"
+    cron_schedule="0,30 11,12,14,16,20,22,0 * * *",execution_timezone="Asia/Bangkok"
 )
 
 defs = Definitions(
