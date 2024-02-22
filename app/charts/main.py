3f46c924ed44370e07a7674edd6881f98e787aa3
luutuankiet <luutuankiet.ftu2@gmail.com>
2024-02-22 13:04:58 +0700

    init code for daily metrics; added tabs

--- app/charts/main.py
+++ app/charts/main.py
@@ -10,6 +10,14 @@ motherduck_token = os.environ.get("motherduck_token")
 con = duckdb.connect(f'md:ticktick_gtd?motherduck_token={motherduck_token}')
 cur = con.cursor()
 
+
+analytics_path = os.path.join(dbt_project_dir,'analyses')
+tags_count_path = os.path.join(analytics_path,'active_tags_count.sql')
+loops_count_path = os.path.join(analytics_path,'open_loops_count.sql')
+active_count_path = os.path.join(analytics_path,'modified_counts.sql')
+
+
+
 def highlight_cell(val):
     if val < 20:
         color = 'yellow' 
@@ -37,218 +45,282 @@ def highlight_text(val):
 
 def highlight_row(row):
     if any(row.astype(str).str.count('-') >= 4):
-         return ['background-color: #D3D3D3; font-weight: bold;'] * len(row)
+        return ['background-color: #D3D3D3; font-weight: bold;'] * len(row)
     else:
         return [''] * len(row)
 
 st.set_page_config(page_title="MY GTD DASHBOARD", layout="wide", initial_sidebar_state="collapsed", menu_items=None)
 
-@st.cache_data
-def get_table(query):
-    return cur.sql(query).df()
+st.header("🌏 Ken's GTD dashboard",divider="blue")
 
-obt=get_table("select * from obt")
-
-
-with st.sidebar:
-    folders = obt['fld_folder_name'].drop_duplicates().to_list()
-    filter_folder = st.multiselect('folders',folders,default=folders)
+tab1,tab2,tab3 = st.tabs(['🧑🏽‍💻 daily ops',
+         '📊 analytics',
+         'placeholder'
+         ])
 
 
-st.write("# at a glance")
-st.write("## count of clarified and next action")
+with tab2:
 
 
-analytics_path = os.path.join(dbt_project_dir,'analyses')
-tags_count_path = os.path.join(analytics_path,'active_tags_count.sql')
+    @st.cache_data
+    def get_table(query):
+        return cur.sql(query).df()
+
+    obt=get_table("select * from obt")
 
 
+    with st.sidebar:
+        folders = obt['fld_folder_name'].drop_duplicates().to_list()
+        filter_folder = st.multiselect('folders',folders,default=folders)
 
-with open(tags_count_path, 'r') as f:
-    tags_query=f.read()
 
-    
-tags_count = get_table(tags_query)
-colored_tags_count = tags_count.style.map(highlight_text,subset=['clarification_progress'])
-tags_count_final = colored_tags_count
-
-st.dataframe(
-    tags_count_final,
-    column_config={
-        "clarification_progress": st.column_config.ProgressColumn(
-        "clarification_progress",
-        format="%f",
-        min_value=0,
-        max_value=100
-    ),
-    },
-
-    hide_index=True,
-    use_container_width=True
-    )
+    st.write("# at a glance")
+    st.write("## count of clarified and next action")
 
 
 
-loops_count_path = os.path.join(analytics_path,'open_loops_count.sql')
 
+    with open(tags_count_path, 'r') as f:
+        tags_query=f.read()
 
+        
+    tags_count = get_table(tags_query)
+    colored_tags_count = tags_count.style.map(highlight_text,subset=['clarification_progress'])
+    tags_count_final = colored_tags_count
 
-with open(loops_count_path, 'r') as f:
-    loops_query=f.read()
+    st.dataframe(
+        tags_count_final,
+        column_config={
+            "clarification_progress": st.column_config.ProgressColumn(
+            "clarification_progress",
+            format="%f",
+            min_value=0,
+            max_value=100
+        ),
+        },
 
-
-loops_count = get_table(loops_query)
-loops = loops_count['content'].loc[0]
-lines = loops.split('\n')
-counter = 0
-for line in lines:
-    if re.search('[a-z0-9]',line.lower()):
-        counter+=1 
-
-compare_clarify = int(tags_count['cnt_clarifyme'].loc[0])
-counter_delta = counter - compare_clarify        
-st.write("## open loops count (rough estimation)")
-st.write()
-col1,col2,col3 = st.columns([1,3,1])
-with col2:
-
-    st.metric(
-        "open loops vs to clarify",
-        value = f"{counter} loops to capture",
-        delta = f"{counter_delta} vs captured loops to clarify",
-        delta_color="inverse",
-        # help="compared to number of items to clarify"
+        hide_index=True,
+        use_container_width=True
         )
 
 
 
 
-st.write("# lvl1-lvl2 analytics")
-st.write("## progress summary")
-lvl1_lvl2_progress = get_table("select * from lvl1_lvl2_progress")
 
-filtered_lvl1_lvl2_progress = lvl1_lvl2_progress[lvl1_lvl2_progress['fld_folder_name'].isin(filter_folder)]
-colored_lvl1_lvl2_progress = filtered_lvl1_lvl2_progress.style.map(
-    highlight_text,subset=['done_progress','clarify_progress']
-).apply(
-    highlight_row,axis=1
-)
-final_lvl1_lvl2_progress = colored_lvl1_lvl2_progress
 
-st.dataframe(
-    final_lvl1_lvl2_progress,
-    column_config={
-        "done_progress": st.column_config.ProgressColumn(
-        "done_progress",
-        format="%f",
-        min_value=0,
-        max_value=100
-    ),
-    "clarify_progress": st.column_config.ProgressColumn(
-        "clarify_progress",
-        format="%f",
-        min_value=0,
-        max_value=100
+
+    st.write("# lvl1-lvl2 analytics")
+    st.write("## progress summary")
+    lvl1_lvl2_progress = get_table("select * from lvl1_lvl2_progress")
+
+    filtered_lvl1_lvl2_progress = lvl1_lvl2_progress[lvl1_lvl2_progress['fld_folder_name'].isin(filter_folder)]
+    colored_lvl1_lvl2_progress = filtered_lvl1_lvl2_progress.style.map(
+        highlight_text,subset=['done_progress','clarify_progress']
+    ).apply(
+        highlight_row,axis=1
     )
-    },
+    final_lvl1_lvl2_progress = colored_lvl1_lvl2_progress
 
-    hide_index=True,
-    use_container_width=True
-    )
-
-
-
-
-st.write('# active plots')
-
-today = datetime.datetime.now()
-this_week_begin = today - datetime.timedelta(days=today.weekday())
-
-start,end = st.date_input(
-    "select the desired week",
-    (this_week_begin,today),
-    max_value=today,
-    format="MM.DD.YYYY"
-)
-
-active_count_path = os.path.join(analytics_path,'modified_counts.sql')
-
-
-with open(active_count_path, 'r') as f:
-    active_query=f.read()
-
-
-active_count = get_table(active_query)
-filtered_active_count = active_count[(active_count['key'] >= pd.to_datetime(start)) & (active_count['key'] <= pd.to_datetime(end))]
-
-st.bar_chart(
-    filtered_active_count,
-    x='day_of_year',y='tasks_active'
-)
-
-
-
-
-st.write("# lvl3 analytics")
-st.write("## summary")
-lvl3_sumarize = get_table("select * from lvl3_sumarize")
-colored_lvl3_sumarize = lvl3_sumarize.style.map(highlight_text,subset=['lvl3_done_progress'])
-lvl3_sumarize_final = colored_lvl3_sumarize
-
-st.dataframe(
-    lvl3_sumarize_final,
-    column_config={
-        "lvl3_done_progress": st.column_config.ProgressColumn(
-        "lvl3_done_progress",
-        format="%f",
-        min_value=0,
-        max_value=100
-    ),
-    },
-
-    hide_index=True,
-    use_container_width=True
-    )
-
-
-
-
-st.write("## detailed")
-lvl3_progress = get_table("select * from lvl3_progress")
-colored_lvl3_progress = lvl3_progress.style.map(highlight_text,subset=['lvl3_done_progress','lvl3_clarify_progress','l_done_progress','l_clarify_progress'])
-lvl3_progress_final = colored_lvl3_progress
-st.dataframe(
-    lvl3_progress_final,
-    column_config={
-        "lvl3_done_progress": st.column_config.ProgressColumn(
-        "lvl3_done_progress",
-        format="%f",
-        min_value=0,
-        max_value=100
-    ),
-    "lvl3_clarify_progress": st.column_config.ProgressColumn(
-        "lvl3_clarify_progress",
-        format="%f",
-        min_value=0,
-        max_value=100
-    ),
-    "l_done_progress": st.column_config.ProgressColumn(
-            "l_done_progress",
+    st.dataframe(
+        final_lvl1_lvl2_progress,
+        column_config={
+            "done_progress": st.column_config.ProgressColumn(
+            "done_progress",
             format="%f",
             min_value=0,
             max_value=100
         ),
-    "l_clarify_progress": st.column_config.ProgressColumn(
-        "l_clarify_progress",
-        format="%f",
-        min_value=0,
-        max_value=100
-    ),
-    },
+        "clarify_progress": st.column_config.ProgressColumn(
+            "clarify_progress",
+            format="%f",
+            min_value=0,
+            max_value=100
+        )
+        },
 
-    hide_index=True,
-    use_container_width=True
+        hide_index=True,
+        use_container_width=True
+        )
+
+
+
+
+    st.write('# active plots')
+
+    today = datetime.datetime.now()
+    this_week_begin = today - datetime.timedelta(days=today.weekday())
+
+    start,end = st.date_input(
+        "select the desired week",
+        (this_week_begin,today),
+        max_value=today,
+        format="MM.DD.YYYY"
     )
+
+
+
+
+    with open(active_count_path, 'r') as f:
+        active_query=f.read()
+
+
+    active_count = get_table(active_query)
+    filtered_active_count = active_count[(active_count['key'] >= pd.to_datetime(start)) & (active_count['key'] <= pd.to_datetime(end))]
+
+    st.bar_chart(
+        filtered_active_count,
+        x='day_of_year',y='tasks_active'
+    )
+
+
+with tab1:
+
+
+    with open(loops_count_path, 'r') as f:
+        loops_query=f.read()
+
+
+    loops_count = get_table(loops_query)
+    loops = loops_count['content'].iloc[0]
+    lines = loops.split('\n')
+    counter = 0
+    for line in lines:
+        if re.search('[a-z0-9]',line.lower()):
+            counter+=1 
+
+    compare_clarify = int(tags_count['cnt_clarifyme'].iloc[0])
+    counter_delta = counter - compare_clarify        
+
+
+    clarifyme_count = tags_count['cnt_clarifyme'].iloc[0]
+    clarifyme_avg = 80 # TODO : count average clarifyme across dataset.
+    delta_clarifyme = clarifyme_avg - clarifyme_count
+    
+    
+    today_table = get_table("""
+                                select 
+                                due_date_id,count(*) as cnt  from 
+                            
+                            (
+                            select * from obt where 
+                            completed_date_id is null
+                            and l_is_active = '1'
+                            and td_kind = 'TEXT'
+                            and fld_folder_name not in ('🚀SOMEDAY lists','🛩Horizon of focus','💤on hold lists')
+                            and l_list_name not like '%tickler note%'                            
+                            ) new
+                                where due_date_id is not null
+                                group by due_date_id
+
+                            """)
+    overdue_count = today_table[today_table['due_date_id'] < pd.to_datetime(datetime.datetime.now().date())]
+    overdue_count = overdue_count['cnt'].iloc[0]
+
+    today_count = today_table[today_table['due_date_id'] == pd.to_datetime(datetime.datetime.now().date())]
+    today_count = today_count['cnt'].iloc[0]
+    today_avg = 8 # TODO : implement average count over dataset.
+    delta_today = today_count - today_avg
+
+    st.write("# your main metrics")
+    col1,col2,col3,col4 = st.columns(4)
+    with col1:
+          st.metric(
+            label="overdue tasks",
+            value=overdue_count
+        )
+    with col2:
+          st.metric(
+            label="tasks lined up",
+            value=overdue_count,
+            delta = f"{delta_today} than usual {today_avg} tasks",
+            delta_color="inverse",
+        )
+
+    with col3:
+        st.metric(
+            "open loops",
+            value = counter,
+            delta = f"{counter_delta} than clarify",
+            delta_color="inverse",
+            # help="compared to number of items to clarify"
+            )
+    with col4:
+          st.metric(
+            label="Clarifyme count",
+            value=clarifyme_count,
+            delta=f'{delta_clarifyme} than weekly average {clarifyme_avg}'
+
+
+
+        )
+
+    # st.write(loops_count,clarifyme_count,overdue_count,today_count)
+    # st.write(pd.to_datetime(today),today_count)
+
+
+
+
+with tab3:
+    st.write("# lvl3 analytics")
+    st.write("## summary")
+    lvl3_sumarize = get_table("select * from lvl3_sumarize")
+    colored_lvl3_sumarize = lvl3_sumarize.style.map(highlight_text,subset=['lvl3_done_progress'])
+    lvl3_sumarize_final = colored_lvl3_sumarize
+
+    st.dataframe(
+        lvl3_sumarize_final,
+        column_config={
+            "lvl3_done_progress": st.column_config.ProgressColumn(
+            "lvl3_done_progress",
+            format="%f",
+            min_value=0,
+            max_value=100
+        ),
+        },
+
+        hide_index=True,
+        use_container_width=True
+        )
+
+
+
+
+    st.write("## detailed")
+    lvl3_progress = get_table("select * from lvl3_progress")
+    colored_lvl3_progress = lvl3_progress.style.map(highlight_text,subset=['lvl3_done_progress','lvl3_clarify_progress','l_done_progress','l_clarify_progress'])
+    lvl3_progress_final = colored_lvl3_progress
+    st.dataframe(
+        lvl3_progress_final,
+        column_config={
+            "lvl3_done_progress": st.column_config.ProgressColumn(
+            "lvl3_done_progress",
+            format="%f",
+            min_value=0,
+            max_value=100
+        ),
+        "lvl3_clarify_progress": st.column_config.ProgressColumn(
+            "lvl3_clarify_progress",
+            format="%f",
+            min_value=0,
+            max_value=100
+        ),
+        "l_done_progress": st.column_config.ProgressColumn(
+                "l_done_progress",
+                format="%f",
+                min_value=0,
+                max_value=100
+            ),
+        "l_clarify_progress": st.column_config.ProgressColumn(
+            "l_clarify_progress",
+            format="%f",
+            min_value=0,
+            max_value=100
+        ),
+        },
+
+        hide_index=True,
+        use_container_width=True
+        )
 
 
 
---
+++ dbt_project/analyses/daily_metrics_draft.sql
@@ -0,0 +1,32 @@
+SELECT
+    due_date_id,
+    
+    fld_folder_name,
+    l_list_name,
+    td_title
+
+    ,*
+    {# COUNT(*) AS cnt #}
+FROM
+    (
+        SELECT
+            *
+        FROM
+            obt
+        WHERE
+            completed_date_id IS NULL
+            AND l_is_active = '1'
+            AND td_kind = 'TEXT'
+            AND fld_folder_name NOT IN (
+                '🚀SOMEDAY lists',
+                '🛩Horizon of focus',
+                '💤on hold lists'
+            )
+            AND l_list_name NOT LIKE '%tickler note%'
+    ) NEW
+WHERE
+    due_date_id IS NOT NULL
+{# GROUP BY
+    due_date_id #}
+    order by 1,2,3
+    
