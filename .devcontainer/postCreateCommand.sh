71b8058b2f80f7c904dea52c22682d30f5d4ca8b
luutuankiet <luutuankiet.ftu2@gmail.com>
2024-05-20 09:48:09 +0000

    added npm to the container

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+##### install npm
+# installs nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# download and install Node.js
+nvm install 20
+
+# verifies the right Node.js version is in the environment
+node -v # should print `v20.13.1`
+
+# verifies the right NPM version is in the environment
+npm -v # should print `10.5.2`
+
+
+
+#### sets up python
 
 apt-get update && apt-get install -y python3-venv
 
@@ -14,7 +30,7 @@ source source_env.sh
 # create env
 python3 -m venv $VIRTUAL_ENV
 
-# add virt env to PATH
+# add virt env to PATH which allows the next part of script to install packages directly to venv
 export PATH="$VIRTUAL_ENV/bin:$PATH"
 
 # install reqs
