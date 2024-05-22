8b917324a684596f93513f8ca9f2a84c99d16d90
luutuankiet <luutuankiet.ftu2@gmail.com>
2024-05-22 04:39:12 +0000

    setup macros

--- dbt_project/macros/coalesce_defaults.sql
+++ dbt_project/macros/coalesce_defaults.sql
@@ -3,10 +3,7 @@
     seed = false
   ) %}
   {%- for col in adapter.get_columns_in_relation(table_name) %}
-    {%- set default_value = 0 if col.data_type == "integer" else '1900-01-01' if col.data_type == "date" else 'default' if col.data_type == "text" else '1900-01-01 00:00:00' if col.data_type == "timestamp without time zone" else false if col.data_type == "boolean" 
-    else '0' if col.data_type == "numeric"
-    else '0.00' if col.data_type == "double precision"
-    else 'default' -%}
+    {%- set default_value = 0 if col.data_type == "integer" else '1900-01-01' if col.data_type == "date" else 'default' if col.data_type == "text" else '1900-01-01 00:00:00' if col.data_type == "timestamp without time zone" else false if col.data_type == "boolean" else '0' if col.data_type == "numeric" else '0.00' if col.data_type == "double precision" else 'default' -%}
     {%- if seed == false %}
       COALESCE(
         "{{ col.name }}",
@@ -15,7 +12,7 @@
     {%- endif %}
 
     {%- if seed == true -%}
-    {{ ' distinct ' if loop.first }}
+      {{ ' distinct ' if loop.first }}
       '{{default_value}}' :: {{ col.data_type }} AS {{ col.name }}
     {% endif %}
 
--- dbt_project/macros/convert_blank_to_nulls.sql
+++ dbt_project/macros/convert_blank_to_nulls.sql
@@ -1,8 +1,10 @@
 -- macros/clean_columns.sql
 {# description : this macro turns blank strings to NULLs. useful for raw data transforms.#}
-{# TODO: update arg as a table to directly parse the table cols instead of having to parse from outside caller #}
-{% macro convert_blank_to_null(columns) %}
-  {% for col in columns %}
+{# usage : SELECT {{convert_balnk_to_nulls('table_name')}} from table_name  #}
+{% macro convert_blank_to_null(
+    table_name
+  ) %}
+  {%- for col in adapter.get_columns_in_relation(table_name) %}
     NULLIF(
       {{ col }},
       ''
