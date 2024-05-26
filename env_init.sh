401d237f51086906ce9a57c71c17bad7930cfbe2
luutuankiet <luutuankiet.ftu2@gmail.com>
2024-05-26 12:53:33 +0000

    done setup dev prod postgres

--- env_init.sh
+++ env_init.sh
@@ -9,7 +9,9 @@ DW_PATH="$ENV_WORK_DIR/database/memory.duckdb"
 DAGSTER_HOME="$ENV_WORK_DIR"
 PYTHONPATH="$ENV_WORK_DIR/app"
 ST_LOGS_PATH="$ENV_WORK_DIR/streamlit_logs/logs.txt"
-motherduck_token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjoibHV1dHVhbmtpZXQuZnR1Mi5nbWFpbC5jb20iLCJlbWFpbCI6Imx1dXR1YW5raWV0LmZ0dTJAZ21haWwuY29tIiwidXNlcklkIjoiMjRkOTQxYzktMDg2OC00ZThmLWIwNjMtNDFmNjE2MDYyMjMxIiwiaWF0IjoxNzA3MTU2NzY2LCJleHAiOjE3Mzg3MTQzNjZ9.z-FjZKJ8oAewQKJ2X3_4emnJXszt1q_VaB0ZVOsQEUM"
+DW_USER="ken"
+DW_PASSWORD="Boyluu96@"
+DW_DBNAME="gtd_dash"
 EOF
 
 # source env
