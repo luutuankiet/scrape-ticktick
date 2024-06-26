a6c8f87a1e55e370dedefc1300b4ad9c15feb649
luutuankiet <luutuankiet.ftu2@gmail.com>
2024-06-26 01:40:46 +0000

    done fact todo

--- dbt_project/models/staging/stg_todos.sql
+++ dbt_project/models/staging/stg_todos.sql
@@ -1,36 +1,64 @@
 WITH todo AS (
     SELECT
-        {{coalesce_defaults(ref('src__tasks_raw'))}}
+        {{ coalesce_defaults(ref('src__tasks_raw')) }}
     FROM
         {{ ref('src__tasks_raw') }}
 ),
 lists AS (
     SELECT
-        {{coalesce_defaults(ref('src__lists_raw'))}}
-        
+        {{ coalesce_defaults(ref('src__lists_raw')) }}
     FROM
         {{ ref('src__lists_raw') }}
 ),
 folders AS (
     SELECT
-        {{coalesce_defaults(ref('src__folders_raw'))}}
-        
+        {{ coalesce_defaults(ref('src__folders_raw')) }}
     FROM
         {{ ref('src__folders_raw') }}
 ),
+statuses AS (
+    SELECT
+        *
+    FROM
+        {{ ref('stg_statuses') }}
+),
+dates AS (
+    SELECT
+        *
+    FROM
+        {{ ref('stg_dates') }}
+),
 joined AS (
     SELECT
+        {{ dbt_utils.generate_surrogate_key(['dds.date_id']) }} AS date_start_key,
+        {{ dbt_utils.generate_surrogate_key(['ddd.date_id']) }} AS date_due_key,
+        {{ dbt_utils.generate_surrogate_key(['ddcm.date_id']) }} AS date_completed_key,
+        {{ dbt_utils.generate_surrogate_key(['ddc.date_id']) }} AS date_created_key
         t.*,
-        coalesce(l.list_name,'default') as list_name,
-        coalesce(f.folder_name,'default') as folder_name
+        l.list_id,
+        f.folder_id,
+        ss.status_id,
     FROM
         todo t
         LEFT JOIN lists l
         ON t.todo_projectid = l.list_id
         LEFT JOIN folders f
         ON l.list_groupid = f.folder_id
+        LEFT JOIN statuses ss
+        ON ss.status_id = t.todo_status
+        LEFT JOIN dates dds
+        ON dds.date_id = t.todo_startdate
+        LEFT JOIN dates ddd
+        ON ddd.date_id = t.todo_duedate
+        LEFT JOIN dates ddc
+        ON ddc.date_id = t.todo_createdtime
+        LEFT JOIN dates ddcm
+        ON ddcm.date_id = t.todo_completedtime
 )
 SELECT
-    {{dbt_utils.generate_surrogate_key(['todo_id'])}} as todo_key,*
+    {{ dbt_utils.generate_surrogate_key(['todo_id']) }} AS todo_key,
+    {{ dbt_utils.generate_surrogate_key(['list_id']) }} AS list_key,
+    {{ dbt_utils.generate_surrogate_key(['folder_id']) }} AS folder_key,
+    {{ dbt_utils.generate_surrogate_key(['status_id']) }} AS status_key,*
 FROM
     joined
