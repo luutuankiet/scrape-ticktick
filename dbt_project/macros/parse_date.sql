e3e0cffaa0652ca8886155e5b08a8d26831b7617
luutuankiet <luutuankiet.ftu2@gmail.com>
2024-06-29 11:56:35 +0000

    checked out create macro parse date

---
+++ dbt_project/macros/parse_date.sql
@@ -0,0 +1,9 @@
+{%- macro parse_date(select_columns) -%}
+    {% set select_columns = select_columns | map("lower") | list %}
+    {% for col in select_columns %}
+        LEFT(
+            "{{col}}" :: text,
+            10
+        )::date AS "{{col}}_derived_date" {{ ", " if not loop.last }}
+    {%- endfor -%}
+{%- endmacro -%}
