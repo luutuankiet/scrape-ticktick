586019e6579eddf2814e83984320adccd53c95c6
luutuankiet <luutuankiet.ftu2@gmail.com>
2024-10-27 07:33:30 +0000

    enhance : text search tokenize more comprehensive with both stop and non stop words

--- dbt_project/macros/setup_textsearch.sql
+++ dbt_project/macros/setup_textsearch.sql
@@ -1,5 +1,23 @@
 {% macro setup_textsearch() %}
   {% set sql %}
+{# setup for config #}
+{# drop the custom config #}
+
+-- prerequisites : shell to postgres & touch /usr/share/postgresql/16/tsearch_data/empty.stop
+
+DROP TEXT SEARCH DICTIONARY IF EXISTS english_nostop CASCADE;
+CREATE TEXT SEARCH DICTIONARY english_nostop (
+    TEMPLATE = pg_catalog.simple,
+    STOPWORDS = 'empty'
+);
+DROP TEXT SEARCH CONFIGURATION IF EXISTS english_nostop_cfg CASCADE ;
+CREATE TEXT SEARCH CONFIGURATION english_nostop_cfg ( COPY = english );
+ALTER TEXT SEARCH CONFIGURATION english_nostop_cfg
+    ALTER MAPPING FOR asciiword, asciihword, hword_asciipart
+    WITH english_nostop, english_stem;
+
+
+
 {# drop the index #}
   DROP INDEX IF EXISTS {{ env_var("TARGET_SCHEMA",'dev') }}.idx_search;
 
@@ -7,7 +25,7 @@
   DROP FUNCTION IF EXISTS {{ env_var("TARGET_SCHEMA",'dev') }}.search_gtd;
 
 ALTER TABLE
-  {{ ref('fact_todos') }} DROP  search CASCADE;
+  {{ ref('fact_todos') }} DROP IF EXISTS search CASCADE;
 
 
 ALTER TABLE
@@ -15,7 +33,19 @@ ALTER TABLE
   --add the search index
 ADD
   search tsvector generated always AS (
-    setweight(to_tsvector('english', todo_title), 'A') || ' ' || setweight(to_tsvector('english', todo_content), 'A') || ' ' || setweight(to_tsvector('english', todo_list_name), 'B') || ' ' || setweight(to_tsvector('english', todo_folder_name), 'B') || ' ' || setweight(to_tsvector('simple', todo_tags), 'C') :: tsvector
+    setweight(to_tsvector('english', todo_title), 'A') || 
+    ' ' || 
+    setweight(to_tsvector('english', todo_content), 'A') || 
+    ' ' || 
+    setweight(to_tsvector('english_nostop_cfg', todo_title), 'B') || 
+    ' ' || 
+    setweight(to_tsvector('english_nostop_cfg', todo_content), 'B') || 
+    ' ' || 
+    setweight(to_tsvector('english_nostop_cfg', todo_list_name), 'B') || 
+    ' ' || 
+    setweight(to_tsvector('english_nostop_cfg', todo_folder_name), 'B') || 
+    ' ' || 
+    setweight(to_tsvector('simple', todo_tags), 'C') :: tsvector
   ) STORED;
 -- add the index
   CREATE INDEX idx_search
@@ -47,7 +77,9 @@ FROM
   {{ ref('fact_todos') }}
 WHERE
   (search @@ websearch_to_tsquery('english', term)
-  OR search @@ websearch_to_tsquery('simple', term))
+  OR search @@ websearch_to_tsquery('simple', term)
+  OR search @@ websearch_to_tsquery('english_nostop_cfg', term)
+  )
   AND todo_status = '0'
 ORDER BY
   RANK DESC;$$ LANGUAGE SQL;
