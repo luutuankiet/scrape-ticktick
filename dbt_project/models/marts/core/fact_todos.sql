3a1664164703b54a4ab8b9e8458d7820a26b8f6e
luutuankiet <luutuankiet.ftu2@gmail.com>
2024-11-23 11:06:07 +0000

    updated incremental config : - `stg_todo`, `fact_todos` now have `is_incremental` config block & postgres based config     - added surrogate key for `fact_todos`     - embedded drop null from `fact_todos` into `stg_todos` - added tests to said models.

--- dbt_project/models/marts/core/fact_todos.sql
+++ dbt_project/models/marts/core/fact_todos.sql
@@ -1,21 +1,8 @@
 {{ config(
     materialized='incremental',
-    unique_key = ['todo_id'],
+    unique_key = ['todo_id', 'date_due_lookahead_key'],
+    incremental_strategy = 'merge',
     on_schema_change='append_new_columns',
-    indexes=[
-      {'columns': ['list_key'], 'type': 'hash'},
-      {'columns': ['folder_key'], 'type': 'hash'},
-      {'columns': ['status_key'], 'type': 'hash'},
-      {'columns': ['date_start_key'], 'type': 'hash'},
-      {'columns': ['date_due_key'], 'type': 'hash'},
-      {'columns': ['date_completed_key'], 'type': 'hash'},
-      {'columns': ['date_created_key'], 'type': 'hash'},
-      {'columns': ['date_modified_key'], 'type': 'hash'},
-      {'columns': ['date_due_lookahead_key'], 'type': 'hash'},
-      {'columns': ['todo_key'], 'unique': True},
-    ],
-    unlogged=True,
-    pre_hook="delete from {{this}} where todo_id is null"
 ) }}
 WITH source AS (
     SELECT
@@ -28,4 +15,7 @@ SELECT
 FROM
     source
 
-    {# dont need a coalesce_defaults at fact table cause the nulls FK already hashed at stg fact  #}
+{% if is_incremental() %}
+  WHERE  todo_modifiedtime >= (select coalesce(max(todo_modifiedtime),'1900-01-01 00:00:00') from {{ this }} )
+{% endif %}
+
