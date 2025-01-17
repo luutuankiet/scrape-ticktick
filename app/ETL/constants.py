f15475bec3133c0b7b28d7a4f7cc40bb2658ff54
luutuankiet <luutuankiet.ftu2@gmail.com>
2025-01-18 04:44:30 +0700

    patch init dbt parse bug on dagster codeoad

--- app/ETL/constants.py
+++ app/ETL/constants.py
@@ -1,21 +1,25 @@
 import os
 from pathlib import Path
 from dagster_dbt import DbtCliResource
+import helper.source_env
 
-DBT_DIR = Path(__file__).joinpath("..","..","..","dbt_project").resolve()
-
+DBT_DIR = Path(__file__).joinpath("..","..","..","dbt_project").resolve(strict=True)
+DBT_TARGET_DIR = DBT_DIR.joinpath("target")
 
 
 dbt = DbtCliResource(project_dir=DBT_DIR,profiles_dir=DBT_DIR)
 
-if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD") == 1:
+if not DBT_TARGET_DIR.exists():
+    # scaffold the project target dir
+    dbt.cli(["deps"], target_path=Path("target")).wait()
+    dbt.cli(["compile"], target_path=Path("target")).wait()
     dbt_manifest_path = (
-        dbt.cli(
-            ["--quiet", "parse"],
-            target_path=Path("target"),
+            dbt.cli(
+                ["--quiet", "parse"],
+                target_path=Path("target"),
+            )
+            .wait()
+            .target_path.joinpath("manifest.json")
         )
-        .wait()
-        .target_path.joinpath("manifest.json")
-    )
 else:
-    dbt_manifest_path = os.path.join(DBT_DIR,"target", "manifest.json")
+    dbt_manifest_path = DBT_DIR.joinpath("target", "manifest.json").resolve(strict=True)
